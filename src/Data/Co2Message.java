9d2e9136b3e51451fecc89b3dc60b8773bacf8cc
Cramblioni <trystanpvincent@gmail.com>
2025-11-22 00:57:02 +0000

    removed unused ByteBuffer based functions

--- src/Data/Co2Message.java
+++ src/Data/Co2Message.java
@@ -1,47 +1,9 @@
 package Data;
 
 import java.io.*;
-import java.nio.ByteBuffer;
 import java.time.LocalDateTime;
 
 public record Co2Message ( String ID, String postcode, float co2ppm ) {
-    public byte[] toBytes() {
-
-        // Using `u16Le` (AFAIK) to store string lengths
-        // Fun
-        assert ID.length() <= 65535;
-        assert postcode.length() <= 65535;
-
-        final int size =
-                  (ID.length() + 2) // including size tag
-                  + (postcode.length() + 2) // including size tag
-                + 4; // The size of a f32
-
-        ByteBuffer output = ByteBuffer.allocate(size);
-        output.putShort((short)ID.length());
-        output.put(ID.getBytes());
-        output.putShort((short)postcode.length());
-        output.put(postcode.getBytes());
-        output.putFloat(co2ppm);
-
-        return output.array();
-    }
-    public static Co2Message fromBytes(byte[] raw) {
-        ByteBuffer reader = ByteBuffer.wrap(raw).asReadOnlyBuffer();
-
-        final int ID_length = reader.getShort();
-        final byte[] ID_raw = new byte[ID_length];
-        reader.get(ID_raw);
-        final String ID = new String(ID_raw);
-
-        final int postcode_length = reader.getShort();
-        final byte[] postcode_raw = new byte[postcode_length];
-        reader.get(postcode_raw);
-        final String postcode = new String(postcode_raw);
-
-        final float co2ppm = reader.getFloat();
-        return new Co2Message(ID, postcode, co2ppm);
-    }
 
     public static Co2Message fromStream(InputStream stream) throws IOException {
         final DataInputStream reader = new DataInputStream(stream);
@@ -76,5 +38,4 @@ public record Co2Message ( String ID, String postcode, float co2ppm ) {
     public TimestampedCo2Record timestamp(LocalDateTime timestamp) {
         return new TimestampedCo2Record(timestamp, ID, postcode, co2ppm);
     }
-};
-;
+}
