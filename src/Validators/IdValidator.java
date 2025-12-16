953b53d7c986ce442db66a9dce8c487d2b7a9dfe
Cramblioni <trystanpvincent@gmail.com>
2025-12-16 12:09:16 +0000

    Moved over to an exception approach when validating
    This should allow us to give a little message when rejecting input.

--- src/Validators/IdValidator.java
+++ src/Validators/IdValidator.java
@@ -2,26 +2,26 @@ package Validators;
 
 import Validators.Exceptions.InvalidIdException;
 
-import java.io.*;
-
 // We're using student IDs as our ID format
 // Essentially ST[0-9]*8
 // :)
 
 public class IdValidator {
     final static int ID_LENGTH = "ST01234567".length();
-    public static boolean valid(String value) {
+    public static void valid(String value) throws InvalidIdException {
         // Length check
-        if (value.length() != ID_LENGTH) return false;
+        if (value.length() != ID_LENGTH)
+            throw new InvalidIdException("ID must be " + ID_LENGTH + " characters long");
 
-        byte[] raw = value.getBytes();
+        char[] raw = value.toCharArray();
 
         // Checking for the "ST"
-        if (raw[0] != 'S' || raw[1] != 'T') return false;
+        if (raw[0] != 'S' || raw[1] != 'T')
+            throw new InvalidIdException("ID must start with \"ST\"");
 
         // Checking if the rest are digits
-        for (int i = 2; i < ID_LENGTH; i += 1)
-            if (!Character.isDigit((char)raw[i])) return false;
-        return true;
+        for (int i = 2; i < ID_LENGTH; i += 1) if (!Character.isDigit((char)raw[i]))
+            throw new InvalidIdException("The final 8 digits of an ID must be digits");
+
     }
 }
--- src/Validators/PostCodeValidator.java
+++ src/Validators/PostCodeValidator.java
@@ -2,8 +2,6 @@ package Validators;
 
 import Validators.Exceptions.PostCodeFormatException;
 
-import java.io.IOException;
-import java.io.PushbackInputStream;
 import java.util.Arrays;
 import java.util.OptionalInt;
 
@@ -12,33 +10,33 @@ import java.util.OptionalInt;
 public class PostCodeValidator {
 
     // ========== validating ==========
-    public static boolean valid(String value) {
+    public static void valid(String value) throws PostCodeFormatException {
         final char[] raw = value.toCharArray();
 
         // Split by space
         final OptionalInt potential_index = findSpace(raw);
-        if (!potential_index.isPresent()) return false;
+        if (!potential_index.isPresent())
+            throw new PostCodeFormatException("Postcodes must contain a space");
+
         final int index = potential_index.getAsInt();
         final char[] potential_outcode = Arrays.copyOfRange(raw, 0, index);
-        final char[] potential_incode = Arrays.copyOfRange(raw, index, raw.length);
+        final char[] potential_incode = Arrays.copyOfRange(raw, index + 1, raw.length);
 
         // Outcodes must be between 2 and 4 characters old, else Fail
         if (potential_outcode.length < 2 || potential_outcode.length > 4)
-            return false;
+            throw new PostCodeFormatException("Outcodes must be between 2 and 4 characters long");
 
         // Outcodes must start with an uppercase letter, else Fail
         if (!Character.isAlphabetic(potential_outcode[0]) || Character.isLowerCase(potential_outcode[0]))
-            return false;
+            throw new PostCodeFormatException("Outcodes must start with an uppercase letter");
 
         // Incodes must be 3 characters long, else Fail
         if (potential_incode.length != 3)
-            return false;
+            throw new PostCodeFormatException("Incodes must be exactly 3 characters long");
 
         // Incodes must start with a number, else Fail
         if (!Character.isDigit(potential_incode[0]))
-            return false;
-
-        return true;
+            throw new PostCodeFormatException("Incode must start with a digit");
     }
     private static OptionalInt findSpace(char[] source) {
         for (int i = 0; i < source.length; i += 1) {
