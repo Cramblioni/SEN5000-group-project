b0914da6ea9517c7f95c467282fe96550519a3cb
Cramblioni <trystanpvincent@gmail.com>
2025-12-16 10:49:41 +0000

    Added gradual input completion to `Client.java`
    The way the client accepts input is now closer to how the documentation
    expects. This requires further cleanup and refinement, but it functions.
    
    The code currently is messy and hard to read, apologies.

--- src/Client.java
+++ src/Client.java
@@ -3,68 +3,113 @@ import Data.Co2Message;
 import java.io.*;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-
-        if (args.length != 5) printUsage("Wrong number of arguments");
-
-        final float reading = validateReading(args[4]);
-
-        short port;
-        try {
-            port = (short)Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            printUsage("Port must be a port number");
-            return;
+        // Splitting args
+        String[] addr_args;
+        String[] data_args;
+        switch (args.length) {
+            case 2: // Address Only
+                addr_args = args;
+                data_args = new String[0];
+                break;
+            case 3: // Data Only
+                addr_args = new String[0];
+                data_args = args;
+                break;
+            case 5: // Both provided
+                addr_args = Arrays.copyOfRange(args, 0, 2);
+                data_args = Arrays.copyOfRange(args, 2, 5);
+                break;
+            default: // Error
+                printUsage("Wrong number of arguments");
+                return;
         }
-
-        /*
-        final Co2Message test = new Co2Message(args[2], args[3], reading);
-        final byte[] raw_test = test.toBytes();
-        System.out.println("record of  " + test);
-        System.out.println("with bytes " + Arrays.toString(raw_test));
-
-        final Co2Message got = Co2Message.fromBytes(raw_test);
-        System.out.println("loaded " + got);
-        */
-        final Co2Message message = new Co2Message(args[2], args[3], reading);
         try {
-            sendUpdate(args[0], port, message);
+            DoYourJob(addr_args, data_args);
         } catch (IOException e) {
-            System.out.println("Failed to send message");
+            System.out.println("Unexpected IO error");
             System.out.println(e.getMessage());
-            return;
         }
-        System.out.println("Message sent successfully");
     }
+    private static void DoYourJob(String[] addr_args, String[] data_args) throws IOException {
+            Socket socket;
+            try {
+                socket = MakeConnection(addr_args);
+            } catch (IOException e) {
+                System.out.println("Failed to connect");
+                System.out.println(e.getMessage());
+                return;
+            }
+            assert socket != null;
 
-    private static void sendUpdate(String address, short port, Co2Message message) throws IOException {
-        try (Socket connection = new Socket(address, port)) {
-            final DataInputStream inStream = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
-            final OutputStream outStream = new BufferedOutputStream(connection.getOutputStream());
-
-            // checking for the OK
-            byte[] code = new byte[2];
-            inStream.readFully(code);
-            if (!Arrays.equals(code, "OK".getBytes())) {
-                System.out.println("Connection Rejected");
+            if (!HandleConnectionStatus(socket)) {
+                System.out.println("Connection rejected");
+                socket.close();
+                return;
             }
 
-            // printing welcome message
-            int length = inStream.readInt();
-            final byte[] welcomeMessage = new byte[length];
-            inStream.readFully(welcomeMessage);
-            System.out.println(new String(welcomeMessage));
+            final Co2Message message = MakeMessage(data_args);
 
-            // finally we send
-            message.intoStream(outStream);
-        };
+            if (!SendUpdate(socket, message)) {
+                System.out.println("Failed to send");
+                socket.close();
+                return;
+            }
+
+            System.out.println("Message sent successfully");
+            socket.close();
+        }
+//        boolean success;
+//        try {
+//            try {
+//                success = sendUpdate(socket, message);
+//            } catch (IOException e) {
+//                System.out.println("Failed to send message");
+//                System.out.println(e.getMessage());
+//                socket.close();
+//                return;
+//            }
+//            socket.close();
+//        } catch (IOException e) {
+//            System.out.println("Closing socket failed");
+//            return;
+//        }
+//        if (success)
+//            System.out.println("Message sent successfully");
+
+    private static boolean HandleConnectionStatus(Socket connection) throws IOException {
+        final DataInputStream inStream = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+
+        byte[] statusBuffer = new byte[2];
+        inStream.readFully(statusBuffer);
+
+        if (!Arrays.equals(statusBuffer, "OK".getBytes())) {
+            return false;
+        }
+
+        // printing welcome message
+        int length = inStream.readInt();
+        final byte[] welcomeMessage = new byte[length];
+        inStream.readFully(welcomeMessage);
+        System.out.println(new String(welcomeMessage));
+        return true;
     }
+    private static boolean SendUpdate(Socket connection, Co2Message message) throws IOException {
+        final OutputStream outStream = new BufferedOutputStream(connection.getOutputStream());
+
+        // finally we send
+        message.intoStream(outStream);
+        outStream.flush();
+        return true;
+    }
+
 
     private static void printUsage(String hint) {
         System.out.println(
-                "\nClient [address] [port] [ID] [email] [reading]\n" +
+                "\nClient [address] [port] [ID] [postcode] [reading]\n" +
                 "\t- [address] : Server address\n" +
                 "\t- [port] : Port to connect to\n" +
                 "\t- [ID] : YOUR id\n" +
@@ -77,12 +122,61 @@ public class Client {
         System.exit(1);
     }
 
-    private static float validateReading(String reading) {
-        try {
-            return Float.parseFloat(reading);
-        } catch (NumberFormatException e) {
-            printUsage("malformed reading");
+    private static Socket MakeConnection(String[] args) throws IOException {
+        String address;
+        String port;
+        Scanner stdin = new Scanner(System.in);
+        if (args.length == 2) {
+            address = args[0];
+            port = args[1];
+        } else {
+            System.out.print("Please input the server's address.\n? ");
+            address = stdin.nextLine().trim();
+            System.out.print("Please input the server's port .\n? ");
+            port = stdin.nextLine().trim();
         }
-        throw new RuntimeException();
+        try {
+            return new Socket(address, Integer.parseInt(port));
+        } catch (NumberFormatException e) {
+            printUsage("Invalid port number (should be an integer between 0 and 65536)");
+            return null; // UNREACHABLE
+        }
     }
+
+    private static Co2Message MakeMessage(String[] args) {
+        if (args == null || args.length == 0) {
+            args = new String[3];
+            GatherMessageArgs(args);
+        }
+
+        //TODO: Validate ID
+        //TODO: Validate postcode
+
+        // Validating Reading
+        float reading;
+        try {
+            reading = Float.parseFloat(args[2]);
+        } catch (NumberFormatException e) {
+            printUsage("Malformed reading (The reading should be a number)");
+            return null; // UNREACHABLE
+        } catch (NullPointerException e) {
+            printUsage("How?");
+            return null; // UNREACHABLE
+        }
+
+        return new Co2Message(args[0], args[1], reading);
+    }
+    private static void GatherMessageArgs(String[] target) {
+        final Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("Enter your ID\n? ");
+        target[0] = inputScanner.nextLine().trim();
+
+        System.out.print("Enter your postcode\n? ");
+        target[1] = inputScanner.nextLine().trim();
+
+        System.out.print("Enter the reading\n? ");
+        target[2] = inputScanner.nextLine().trim();
+    }
+
 }
